3f3a9f05c79329e0b3d7cacace3b0c8c76d4e5e0
Danil <Dpopov25@yandex.ru>
2025-03-22 16:27:58 +0500

    ref

---
+++ cpp/LabWork4_Task1/LabWork4_Task1.cpp
@@ -0,0 +1,34 @@
+﻿using namespace std;
+#include <iostream>
+#include <string.h>
+
+int getStringLen(char* inputString) {
+    int i = 0;
+    while (inputString[i])
+        i++;
+    
+    return i;
+}
+
+char* copyString(char* destination, char* source) {
+    destination = source;
+
+    return destination;
+}
+
+int main()
+{
+    char myString [23] = "myString";
+    char mySecondString[15] = "mySecondString";
+    char myThirdString[15];
+
+    cout << "Длина строки - " << strlen(myString);
+    cout << "Объединенные строки - " << strcat_s(myString, mySecondString);
+    cout << "Скопированная строка - " << strcpy_s(myThirdString, mySecondString);
+    cout << "Сравнение строк myString и myThirdString - " << strcmp(myString, myThirdString);
+
+    cout << "Длина строки из своей функции - " << getStringLen(myString);
+
+    return 0;
+}
+
